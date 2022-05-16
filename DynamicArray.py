7d8ffae05261830da6020a93791ddcfde8d5708f
Zhaoxin <77062275+Zhaoxin-Guo@users.noreply.github.com>
2022-05-16 09:17:07 +0800

    Update DynamicArray.py

--- DynamicArray.py
+++ DynamicArray.py
@@ -1,6 +1,7 @@
 import ctypes
 import copy
 
+
 class DynamicArray(object):
     def __init__(self, capacity=10, grow_factor=2):
         self._grow_factor = grow_factor
@@ -46,7 +47,6 @@ class DynamicArray(object):
     def _make_array(self, c):
         return (c * ctypes.py_object)()
 
-
     def _add(self, value):
         if self._size == self._capacity:
             self._resize(self._grow_factor * self._capacity)
@@ -61,6 +61,7 @@ class DynamicArray(object):
         self._array = B
         self._capacity = c
 
+        
 def cons(array, value):
     if type(array) is not DynamicArray:
         new_array = DynamicArray()
@@ -69,10 +70,12 @@ def cons(array, value):
     res._add(value)
     return res
 
+
 def length(self):
     if (self is None):
         return 0
     return self._size
+
 
 def insert(self, index, value):
     new_array = copy.deepcopy(self)
@@ -84,6 +87,7 @@ def insert(self, index, value):
     new_array._array[index] = value
     new_array._size += 1
     return new_array
+
 
 def to_list(array):
     if array is None:
@@ -100,11 +104,13 @@ def from_list(array):
         res._add(i)
     return res
 
+
 def find(array, f):
     for i in array:
         if f(i):
             return True
     return False
+
 
 def remove(array, idx):
     if idx < 0 or idx >= array._size:
@@ -119,12 +125,14 @@ def remove(array, idx):
             return new_array
     raise AssertionError('value not found')
 
+    
 def member(array, value):
     """ Gets array elements based on index """
     for i in range(array._size):
         if(array._array[i] == value):
             return True
     return False
+
 
 def filter(self, f):
     res = []
@@ -133,11 +141,13 @@ def filter(self, f):
             res.append(self._array[i])
     return res
 
+
 def map(self, f):
     new_array = copy.deepcopy(self)
     for i in range(new_array._size):
         new_array._array[i] = f(new_array._array[i])
     return new_array
+
 
 def reduce(self, f, initial_state):
     state = initial_state
@@ -145,16 +155,20 @@ def reduce(self, f, initial_state):
         state = f(state, self._array[i])
     return state
 
+
 def empty(self):
     return DynamicArray()
+
 
 def reverse(array):
     lst = to_list(array)
     lst.reverse()
     return from_list(lst)
 
+
 def iterator(array):
     return iter(array)
+
 
 def concat(array1, array2):
     res = DynamicArray()
