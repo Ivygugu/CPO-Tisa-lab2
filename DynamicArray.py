3dfb4525e1122d055adfca817c35c58c6bcff48f
bChen <1377681089@qq.com>
2022-06-03 16:53:15 +0800

    Update DynamicArray.py

--- DynamicArray.py
+++ DynamicArray.py
@@ -1,26 +1,25 @@
 import ctypes
 import copy
 
-
 class DynamicArray(object):
-    def __init__(self, capacity=10, grow_factor=2):
+    def __init__(self, capacity=10, grow_factor=2) -> None:
         self._grow_factor = grow_factor
         self._size = 0
-        self._capacity = capacity  # Initialize chunk size to 10
+        self._capacity = capacity
         self._start = 0
         self._array = [None] * self._capacity
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self._size
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self._array[:self._size])
 
-    def __iter__(self):
+    def __iter__(self) -> 'DynamicArray':
         array = from_list(to_list(self))
         return array
 
-    def __next__(self):
+    def __next__(self) -> int:
         if self._start <= self._size - 1:
             res = self._array[self._start]
             self._start += 1
@@ -28,7 +27,7 @@ class DynamicArray(object):
         else:
             raise StopIteration
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         if not isinstance(other, DynamicArray):
             return False
         if self._capacity != other._capacity:
@@ -44,25 +43,24 @@ class DynamicArray(object):
             return flag
         return False
 
-    def _make_array(self, c):
+    def _make_array(self, c) -> 'DynamicArray':
         return (c * ctypes.py_object)()
 
-    def _add(self, value):
+    def _add(self, value) -> None:
         if self._size == self._capacity:
             self._resize(self._grow_factor * self._capacity)
         self._array[self._size] = value
         self._size += 1
 
-    def _resize(self, c):
+    def _resize(self, c) -> None:
         B = self._make_array(c)
-        # 扩充之后，需要将原数组中的元素copy到新数组中
         for k in range(self._size):
             B[k] = self._array[k]
         self._array = B
         self._capacity = c
 
 
-def cons(array, value):
+def cons(array, value) -> 'DynamicArray':
     if type(array) is not DynamicArray:
         new_array = DynamicArray()
         return cons(new_array, value)
@@ -70,14 +68,12 @@ def cons(array, value):
     res._add(value)
     return res
 
-
-def length(self):
+def length(self) -> int:
     if (self is None):
         return 0
     return self._size
 
-
-def insert(self, index, value):
+def insert(self, index, value) -> 'DynamicArray':
     new_array = copy.deepcopy(self)
 
     if new_array._size == new_array._capacity:
@@ -88,8 +84,7 @@ def insert(self, index, value):
     new_array._size += 1
     return new_array
 
-
-def to_list(array):
+def to_list(array) -> list:
     if array is None:
         return []
     res = []
@@ -98,21 +93,19 @@ def to_list(array):
     return res
 
 
-def from_list(array):
+def from_list(array) -> 'DynamicArray':
     res = DynamicArray()
     for i in array:
         res._add(i)
     return res
 
-
-def find(array, f):
+def find(array, f) -> bool:
     for i in array:
         if f(i):
             return True
     return False
 
-
-def remove(array, idx):
+def remove(array, idx) -> 'DynamicArray':
     if idx < 0 or idx >= array._size:
         raise Exception('the index is not in Dynamic array')
     new_array = copy.deepcopy(array)
@@ -125,52 +118,44 @@ def remove(array, idx):
             return new_array
     raise AssertionError('value not found')
 
-
-def member(array, value):
+def member(array, value) -> bool:
     """ Gets array elements based on index """
     for i in range(array._size):
         if(array._array[i] == value):
             return True
     return False
 
-
-def filter(self, f):
+def filter(self, f) -> list:
     res = []
     for i in range(self._size):
         if (f(self._array[i])):
             res.append(self._array[i])
     return res
 
-
-def map(self, f):
+def map(self, f) -> 'DynamicArray':
     new_array = copy.deepcopy(self)
     for i in range(new_array._size):
         new_array._array[i] = f(new_array._array[i])
     return new_array
 
-
-def reduce(self, f, initial_state):
+def reduce(self, f, initial_state) -> int:
     state = initial_state
     for i in range(self._size):
         state = f(state, self._array[i])
     return state
 
-
-def empty(self):
+def empty(self) -> 'DynamicArray':
     return DynamicArray()
 
-
-def reverse(array):
+def reverse(array) -> 'DynamicArray':
     lst = to_list(array)
     lst.reverse()
     return from_list(lst)
 
-
-def iterator(array):
+def iterator(array) -> 'DynamicArray':
     return iter(array)
 
-
-def concat(array1, array2):
+def concat(array1, array2) -> 'DynamicArray':
     res = DynamicArray()
     for i in range(array1._size):
         res._add(array1._array[i])
